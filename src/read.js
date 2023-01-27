da6de8abb3177e6f4f2871fe023f270c11508d5c
unknown <koszegi.proba9@gmail.com>
2023-01-27 23:33:42 +0100

    Connected with MongoDB and read function created

--- src/read.js
+++ src/read.js
@@ -1,9 +1,8 @@
 async function readData() {
-  const response = await fetch("http://127.0.0.1:4000/read");
-  const fetchedItems = await response.json();
-  if (fetchedItems) {
-    return fetchedItems;
-  }
+  const results = await fetch("/.netlify/functions/db_read").then((response) =>
+    response.json()
+  );
+  return results;
 }
 
 export default readData;
