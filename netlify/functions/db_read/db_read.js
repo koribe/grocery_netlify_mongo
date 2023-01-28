e1d509e379dbc89ece2c6ab48034f663b3d9b349
unknown <koszegi.proba9@gmail.com>
2023-01-28 22:48:42 +0100

    db_read function connection changed

--- netlify/functions/db_read/db_read.js
+++ netlify/functions/db_read/db_read.js
@@ -1,4 +1,45 @@
-const { MongoClient } = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = "Gyakorlas";
+
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+  // we can cache the access to our database to speed things up a bit
+  // (this is the only thing that is safe to cache here)
+  if (cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = client.db(DB_NAME);
+
+  return cachedDb;
+};
+
+const queryDatabase = async (db) => {
+  const pokemon = await db.collection("grocery").find({}).toArray();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(pokemon),
+  };
+};
+
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+  return queryDatabase(db);
+};
+/*const { MongoClient } = require("mongodb");
 
 const mongoClient = new MongoClient(process.env.MONGODB_URI);
 
@@ -18,4 +59,4 @@ const handler = async (event) => {
   }
 };
 
-module.exports = { handler };
+module.exports = { handler };*/
