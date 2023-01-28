aff68f12b3be02605eeb63375e25a33571c6b61f
unknown <koszegi.proba9@gmail.com>
2023-01-28 21:53:06 +0100

    read.js changed to await

--- src/read.js
+++ src/read.js
@@ -1,8 +1,9 @@
 async function readData() {
-  const results = await fetch("/.netlify/functions/db_read").then((response) =>
-    response.json()
-  );
-  return results;
+  const response = await fetch("/.netlify/functions/db_read");
+  const fetchedItems = await response.json();
+  if (fetchedItems) {
+    return fetchedItems;
+  }
 }
 
 export default readData;
