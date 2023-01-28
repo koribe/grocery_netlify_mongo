b13350e411ef6f46afa13564cd7cf60cb6e7d595
unknown <koszegi.proba9@gmail.com>
2023-01-28 22:03:36 +0100

    read.js try catch added

--- src/read.js
+++ src/read.js
@@ -1,8 +1,22 @@
 async function readData() {
-  const response = await fetch("/.netlify/functions/db_read");
-  const fetchedItems = await response.json();
-  if (fetchedItems) {
+  try {
+    const response = await fetch("/.netlify/functions/db_read");
+    if (!response.ok) {
+      return {
+        statusCode: response.status,
+        body: `MongoDB read function fetch error: ${response.statusText}`,
+      };
+    }
+    const fetchedItems = await response.json();
     return fetchedItems;
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `Handler function error: ${error.message}`,
+      }),
+    };
   }
 }
 
